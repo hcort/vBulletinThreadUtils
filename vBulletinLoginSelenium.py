16cf9cda81ce0c29f3ffbea5e4e1520e38d4e069
Hector <h.cortiguera@gmail.com>
2022-02-11 14:25:55 +0100

    Delete obsolete code and add some safer code

--- vBulletinLoginSelenium.py
+++ vBulletinLoginSelenium.py
@@ -1,7 +1,4 @@
-import re
-
 import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -9,32 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def test_requests(driver, current_url):
-    headers = {
-        "User-Agent":
-            "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-    }
-    s = requests.session()
-    s.headers.update(headers)
-
-    for cookie in driver.get_cookies():
-        c = {cookie['name']: cookie['value']}
-        s.cookies.update(c)
-
-    # driver.close()
-
-    current_page = s.get(current_url)
-    if current_page.status_code != requests.codes.ok:
-        return
-    soup = BeautifulSoup(current_page.text, features="html.parser")
-    regex_id = re.compile("edit([0-9]{9})")
-    all_posts = soup.find_all('div', id=regex_id, recursive=True)
-    if all_posts:
-        print('Encuentro posts')
-
-
-def VBulletinLoginSelenium(login_url='', login_data={}):
-    if not login_url:
+def VBulletinLoginSelenium(login_url='', login_data=None):
+    if not login_url or not login_data:
         return None
     driver = webdriver.Firefox()
     # selenium.common.exceptions.WebDriverException: Message: Service geckodriver unexpectedly exited. Status code
@@ -42,16 +15,17 @@ def VBulletinLoginSelenium(login_url='', login_data={}):
     # or isn't valid in this context > actualizar geckodriver https://stackoverflow.com/a/70822145/4105601
     driver.get(login_url)
     element = driver.find_element(By.ID, "navbar_username")
-    element.send_keys(login_data['vb_login_username'])
+    element.send_keys(login_data.get('vb_login_username', ''))
     element = driver.find_element(By.ID, "navbar_password")
-    element.send_keys(login_data['vb_login_password'])
+    element.send_keys(login_data.get('vb_login_password', ''))
     element.send_keys(Keys.RETURN)
-    timeout = 1000
-    element_present = EC.presence_of_element_located((By.LINK_TEXT, login_data['vb_login_username']))
+    timeout = 10
+    element_present = EC.presence_of_element_located((By.LINK_TEXT, login_data.get('vb_login_username', '')))
     WebDriverWait(driver, timeout).until(element_present)
     headers = {
         "User-Agent":
-            "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+            "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 "
+            "Safari/537.36 "
     }
     s = requests.session()
     s.headers.update(headers)
